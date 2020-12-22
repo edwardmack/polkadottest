eacc0613fafdc3312d194c7c9d1336cfb9ba2308
Edward Mack <ed@edwardmack.com>
2020-12-22 18:30:34 -0500

    save commit

--- balanceQuery.js
+++ balanceQuery.js
@@ -3,80 +3,20 @@ const { ApiPromise, WsProvider } = require('@polkadot/api');
 
 // Known account we want to use (available on dev chain, with funds)
 const Alice = '5GrwvaEF5zXb26Fz9rcQpDWS57CtERHpNehXCPcNoHGKutQY';
-const Bob = '5FHneW46xGXgs5mUiveU4sbTyGBzmstUspZC92UhjJM694ty';
-const TestAcct = '5CXrQshxdeXW1tjQf7h8WickrTBWQ1t5LYPqoDnqvPUYNzSf';
-const Test2 = '5ENZyMWrjcmP9apHqpQBfA9ifEof85GNbPE58RWMN1KQ9jPL';
 
 async function main () {
-	const provider = new WsProvider('ws://127.0.0.1:8546');
 	//const provider = new WsProvider('ws://127.0.0.1:9944');
+	const provider = new WsProvider('ws://127.0.0.1:8546');
 
 	// Create an await for the API
   	const api = await ApiPromise.create({provider});
 
-	//let accountNonce = await api.query.system.accountNonce(Alice);
-	//console.log(`acc nonce ${accountNonce}`);
-
-	//extCount = await api.query.system.extrinsicCount();
-	//console.log(`extCount ${JSON.stringify(extCount)}`);
-
-	//allExt = await api.query.system.allExtrinsicsWeight();
-	//console.log(`all extrinsic weight ${JSON.stringify(allExt)}`);
-
-	//blockHash = await api.query.system.blockHash("1");
-
-	//console.log(`block hash ${JSON.stringify(blockHash)}`);
-
-	//number = await api.query.system.number();
-	//console.log(`number ${JSON.stringify(number)}`);
-
-	await sleep(1000)
-//	babeAuths = await api.query.babe.authorities();
-//	console.log(`babe auths ${JSON.stringify(babeAuths)}`);
-
-	totalIss = await api.query.balances.totalIssuance();
-	console.log(`totalIss ${JSON.stringify(totalIss)}`);
-
-	await sleep(1000)
-	freeBal = api.query.system.account(Alice);
-	//freeBal = await api.query.balances.freeBalance(Alice);
+	freeBal = await api.query.system.account(Alice);
 	console.log(`Alice free bal ${JSON.stringify(freeBal)}`);
-/*	
-	await sleep(1000)
-	freeBalBob = await api.query.balances.freeBalance(Bob);
-	console.log(`Bob free bal ${JSON.stringify(freeBalBob)}`);
-	
-	await sleep(1000)
-	freeBalTestAcct = await api.query.balances.freeBalance(TestAcct);
-	console.log(`TestAcct free bal ${JSON.stringify(freeBalTestAcct)}`);
-	
-	await sleep(1000)
-	freeBalTest2 = await api.query.balances.freeBalance(Test2);
-	console.log(`Test2 free bal ${JSON.stringify(freeBalTest2)}`);
-*/
-/*
-  // Retrieve the initial balance. Since the call has no callback, it is simply a promise
-  // that resolves to the current on-chain value
-  let { data: { free: previousFree }, nonce: previousNonce } = await api.query.balances.totalissuance();
 
-  console.log(`${Alice} has a balance of ${previousFree}, nonce ${previousNonce}`);
-  console.log(`You may leave this example running and start example 06 or transfer any value to ${Alice}`);
-
-  // Here we subscribe to any balance changes and update the on-screen value
-  api.query.system.account(Alice, ({ data: { free: currentFree }, nonce: currentNonce }) => {
-    // Calculate the delta
-    const change = currentFree.sub(previousFree);
-
-    // Only display positive value changes (Since we are pulling `previous` above already,
-    // the initial balance change will also be zero)
-    if (!change.isZero()) {
-      console.log(`New balance change of ${change}, nonce ${currentNonce}`);
-
-      previousFree = currentFree;
-      previousNonce = currentNonce;
-    }
-  });
-  */
+	const hash = await api.rpc.chain.getBlockHash(1)
+	const block = await api.rpc.chain.getBlock(hash)
+	console.log(block.header)
 }
 
 main().catch(console.error);
--- transBalance.js
+++ transBalance.js
@@ -15,59 +15,22 @@ async function main () {
 	//const provider = new WsProvider('ws://127.0.0.1:9944');
 
 	// Create an await for the API
-  	const api = await ApiPromise.create({provider});
+  const api = await ApiPromise.create({provider});
 
 	const keyring = new Keyring({type: 'sr25519' });
 	const alice = keyring.addFromUri('//Alice');
 
 	const transfer = api.tx.balances.transfer(BOB, 12345);
-	console.log("sending");
-	const hash = await transfer.signAndSend(alice);
+	console.log(`sending ${transfer}`);
 
-	console.log('trans hash', hash.toHex());
+  // Sign and send the transaction using our account
+  const hash = await transfer.signAndSend(alice);
 
-// Subscribe to balance changes for our account
-// const unsub = await api.query.system.account(Alice, ({ nonce, data: balance }) => {
-//   console.log(`free balance is ${balance.free} with ${balance.reserved} reserved and a nonce of ${nonce}`);
-// });
+  console.log('Transfer sent with hash', hash.toHex());
 
-	// const {nonce} = await api.query.system.account(Alice);
-
-	let nonce = await api.query.system.accountNonce(Alice);
-        console.log(`acc nonce ${nonce}`);
-
-	const recipient = keyring.addFromSeed(randomAsU8a(32)).address;
-
-  // // await sleep(1000)
-	//  freeBal = await api.query.balances.freeBalance(Alice);
-  //  console.log(`Alice free bal ${JSON.stringify(freeBal)}`);
+  aliceBal = await api.query.system.account(Alice);
+  console.log(`Alice free bal ${JSON.stringify(aliceBal)}`);
   
-	
-	console.log(`keyring ${keyring}`);
-	console.log(`alice keypair ${alicePair}`);
-	console.log(`recipient  ${recipient}`);
-console.log('Sending', AMOUNT, 'from', alicePair.address, 'to', recipient, 'with nonce', nonce.toString());
-
-  // Do the transfer and track the actual status
-  api.tx.balances
-    .transfer(recipient, AMOUNT)
-    .signAndSend(alicePair, { nonce }, ({ events = [], status }) => {
-      console.log('Transaction status:', status.type);
-
-      if (status.isInBlock) {
-        console.log('Included at block hash', status.asInBlock.toHex());
-        console.log('Events:');
-
-        events.forEach(({ event: { data, method, section }, phase }) => {
-          console.log('\t', phase.toString(), `: ${section}.${method}`, data.toString());
-        });
-      } else if (status.isFinalized) {
-        console.log('Finalized block hash', status.asFinalized.toHex());
-
-        process.exit(0);
-      }
-    });
-
 
 }
 
